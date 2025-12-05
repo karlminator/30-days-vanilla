4e5c5aa3a6615efaf612cab4360b7eed6ce163a9
karlminator <kf.holmsten@gmail.com>
2025-12-05 10:28:54 +0100

    setup 6, and a fetch

---
+++ 06-type-ahead/main.js
@@ -0,0 +1,19 @@
+const cities = [];
+
+async function getData() {
+    const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+  try {
+    const response = await fetch(endpoint);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+const data = await getData();
+console.log(data);
